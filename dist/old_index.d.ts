59aeff83f3619dd2fb8e3220742c371adfced899
Brayden Moon <crazywolf132@gmail.com>
2022-06-08 14:55:55 +1000

    fix: fixed package

--- dist/old_index.d.ts
+++
@@ -1,9 +0,0 @@
-declare type EventType = string;
-declare type Handler<T = unknown> = (event: T) => void;
-declare const _default: <Events extends Record<string, unknown>>() => {
-    on: (type: EventType, handler: Handler<Events[keyof Events]>) => void;
-    once: (type: EventType, handler: Handler<Events[keyof Events]>) => void;
-    off: (type: EventType, handler?: Handler<Events[keyof Events]> | undefined) => void;
-    emit: (type: EventType, evt?: Events[string] | undefined) => void;
-};
-export default _default;
